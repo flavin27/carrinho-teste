5295e51bac9529498d34d2375911406808fa23fd
Flavio <77132531+flaviodev27@users.noreply.github.com>
2023-01-20 07:31:21 -0300

    added input

--- main.py
+++ main.py
@@ -2,17 +2,27 @@ from classe import Carrinho, Produto
 
 carrinho = Carrinho()
 
-p1 = Produto('camisa', 50)
-p2 = Produto('chocolate', 10)
-p3 = Produto('PS4', 2000)
+resposta = input('Deseja adicionar um item ao carrinho? ')
+c = 0
+while resposta.lower() != 'nao':
+    c += 1
+    compra = input('Qual o nome do produto que deseja comprar?')
+    preco = float(input('Qual o valor desse produto? '))
+    produto = Produto(compra, preco)
+    carrinho.adicionar_produto(produto)
+    resposta = input('Deseja adicionar mais um item ao carrinho? ')
+    if resposta.lower() == 'nao':
+        resposta2 = input('Deseja retirar algum produto do carrinho? ')
+        if resposta2.lower() == 'sim':
+            carrinho.mostrar_produtos()
+            resposta3 = int(input('Qual produto deseja retirar(informe seu número na lista): '))
+            carrinho.remover_produto(resposta3)
+            resposta2 = input('Deseja retirar mais algum produto do carrinho? ')
+        carrinho.mostrar_produtos()
+        print(f'A soma total é de: R${carrinho.soma_total()}')
 
-carrinho.adicionar_produto(p1)
-carrinho.adicionar_produto(p2)
-carrinho.adicionar_produto(p3)
-carrinho.mostrar_produtos()
-print(carrinho.soma_total())
-print('-' * 50)
-carrinho.remover_produto(0)
-carrinho.remover_produto(1)
-carrinho.mostrar_produtos()
-print(carrinho.soma_total())
+
+
+
+
+
